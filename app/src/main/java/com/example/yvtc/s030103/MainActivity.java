8652f135e531456247b6c8353e01f732ce0cf62b
dannielccw <dannielccw@gmail.com>
2017-03-01 13:25:10 +0800

    test

--- app/src/main/java/com/example/yvtc/s030103/MainActivity.java
+++ app/src/main/java/com/example/yvtc/s030103/MainActivity.java
@@ -4,7 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-
+//dan.w
 public class MainActivity extends AppCompatActivity {
 
     @Override
