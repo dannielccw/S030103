c58772061aabe9de97199497413f2c86cb5788f6
dannielccw <dannielccw@gmail.com>
2017-03-01 15:36:59 +0800

    test

--- app/src/main/java/com/example/yvtc/s030103/MainActivity.java
+++ app/src/main/java/com/example/yvtc/s030103/MainActivity.java
@@ -7,7 +7,7 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
-//dan.w: test
+//dan.w: test2
 public class MainActivity extends AppCompatActivity {
 
     @Override
